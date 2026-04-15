3464fe7eb565590dd12737999f46a72c8104cc19
andrew <andrew@collectivelab.io>
2026-04-15 17:41:23 +0300

    Update Omnigraph formula to v0.2.2

--- Formula/omnigraph.rb
+++ Formula/omnigraph.rb
@@ -2,21 +2,21 @@ class Omnigraph < Formula
   desc "Typed property graph database with Git-style workflows"
   homepage "https://github.com/ModernRelay/omnigraph"
   license "MIT"
-  version "0.1.0"
+  version "0.2.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ModernRelay/omnigraph/releases/download/v0.1.0/omnigraph-macos-arm64.tar.gz"
-      sha256 "96aadeb1a5c9b556ee2311225bb2beb5b5bf6fb7d817d959aca856dc57fd6f07"
+      url "https://github.com/ModernRelay/omnigraph/releases/download/v0.2.2/omnigraph-macos-arm64.tar.gz"
+      sha256 "53422374e91625ad23c3fdcacc703fda37aafe948f077cc05d45911f752d9e4c"
     else
-      url "https://github.com/ModernRelay/omnigraph/releases/download/v0.1.0/omnigraph-macos-x86_64.tar.gz"
-      sha256 "8a83ddd9675760e4e224810b437dbcd8fd0675f4399d21f6e9fe31f59491edd7"
+      url "https://github.com/ModernRelay/omnigraph/releases/download/v0.2.2/omnigraph-macos-x86_64.tar.gz"
+      sha256 "1cd3ae57efb0fddca3ed772a9d56045b8e6dba7f59914af07c74417e50d43d1e"
     end
   end
 
   on_linux do
-    url "https://github.com/ModernRelay/omnigraph/releases/download/v0.1.0/omnigraph-linux-x86_64.tar.gz"
-    sha256 "34f73bcf12f38f9f08e413986714e9ae66116d56646e173c52b8be205c869b8d"
+    url "https://github.com/ModernRelay/omnigraph/releases/download/v0.2.2/omnigraph-linux-x86_64.tar.gz"
+    sha256 "5c696fd217c18968a331545964d7b6082ba0a58487abf4c02ace7bc670b5e56d"
   end
 
   head "https://github.com/ModernRelay/omnigraph.git", branch: "main"
